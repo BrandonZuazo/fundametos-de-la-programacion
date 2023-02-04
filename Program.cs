3d409011d39e6af45eef8d64d2570581f994622b
Brandon Zuazo <brandon.zuazo.et7@gmail.com>
2023-02-04 13:38:54 -0300

    subida 1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int   kciudades, velocidad , tiempo;
+            Console.WriteLine("ingrese la distancia de ambas ciudades");
+            kciudades = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese la velocidad promedio");
+            velocidad = int.Parse(Console.ReadLine());
+            tiempo = kciudades / velocidad;
+            Console.WriteLine("el tiempo aproximado es: "+ tiempo);
+        }
+    }
+}
